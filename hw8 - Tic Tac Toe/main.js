e35b7f59c872b9d4960b9848d6f3e561260053eb
Christian <the7christian@gmail.com>
2020-11-13 21:04:34 +0100

    v 2
    remove the if condition

--- hw8 - Tic Tac Toe/main.js
+++ hw8 - Tic Tac Toe/main.js
@@ -1,16 +1,15 @@
+
 // DUA --- <img src='https://i.ibb.co/6Bzp71H/dua.png' width='245' height='200'>
+// Taking all the divs elements inside of board
+const com = document.getElementsByClassName("Board")[0];
+let idVal = com.getElementsByClassName("shape").length;
+com.addEventListener("click", LogEvent);
 
-const target = document.getElementsByClassName("Board");
-const clickDiv = target[0];
-
-clickDiv.addEventListener("click", LogEvent);
-
+// The function
 function LogEvent(event){
-  console.log(event.target)
 
-  if(event.target.id==="one"){
-    console.log("HAHAHAHHAHAHAHA")
-    document.getElementById("one").innerHTML = " X ";
-  }
+  console.log(event.target.id)
+  document.getElementById(event.target.id).innerHTML = " FUCK ";
+  console.log(idVal)
 
 }
