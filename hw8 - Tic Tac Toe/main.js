88ebb3ccb99eb8938a0cb642df3fb68818708fd7
Christian <the7christian@gmail.com>
2020-11-13 21:55:01 +0100

    v1.0

--- hw8 - Tic Tac Toe/main.js
+++ hw8 - Tic Tac Toe/main.js
@@ -2,14 +2,19 @@
 // DUA --- <img src='https://i.ibb.co/6Bzp71H/dua.png' width='245' height='200'>
 // Taking all the divs elements inside of board
 const com = document.getElementsByClassName("Board")[0];
-let idVal = com.getElementsByClassName("shape").length;
+let idVal = com.getElementsByClassName("shape");
+
+let vacia = idVal[0].textContent;
+
 com.addEventListener("click", LogEvent);
 
 // The function
 function LogEvent(event){
 
-  console.log(event.target.id)
-  document.getElementById(event.target.id).innerHTML = " FUCK ";
-  console.log(idVal)
+    console.log(event.target.id)
+
+    document.getElementById(event.target.id).innerHTML = " FUCK ";
+
+    console.log(idVal[7].textContent)
 
 }
