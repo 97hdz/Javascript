d0cf4f8968724c7fd1c877a6ea71b701e2c94ca2
97hdz <70217263+97hdz@users.noreply.github.com>
2020-11-20 22:59:33 +0100

    version numero 2

--- Project #1 - to do list/script.js
+++ Project #1 - to do list/script.js
@@ -4,15 +4,23 @@ const signUp = document.getElementById('sign-up');
 let hiddenLog = document.getElementById('loghidden');
 let hiddenSign = document.getElementById('signhidden');
 
-// ---------- Every click -----
-const logInClick = (event) => {
-  hiddenLog.removeAttribute("hidden", {once:true});
+// ---------- Every click
+const hideLog = (event) => {
+  hiddenLog.removeAttribute("hidden")
+  if (hiddenSign.attributes[0] == 'hidden'){
+  } else {
+    hiddenSign.setAttribute("hidden")
+  }
 }
 
-const SignUpClick = (event) => {
-  hiddenSign.removeAttribute("hidden", {once:true});
+const hideSign = (event) => {
+  hiddenSign.removeAttribute("hidden")
+  if (hiddenLog.attributes[0] == 'hidden'){
+  } else {
+    hiddenLog.setAttribute("hidden")
+  }
 }
 
-// ---------- Listeners ----
-logIn.addEventListener('click', logInClick)
-signUp.addEventListener('click', SignUpClick)
+// ---------- Listeners
+logIn.addEventListener('click', hideLog)
+signUp.addEventListener('click', hideSign)
