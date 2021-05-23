0d38f2cfa75ffb411077560061946e4e9a6c98f7
97hdz <70217263+97hdz@users.noreply.github.com>
2021-05-23 23:37:16 +0200

    Add files via upload
    first version

---
+++ JS To-Do-List/main.js
@@ -0,0 +1,7 @@
+//Listeners
+let signupButton = document.getElementById("signupButton");
+let loginButton = document.getElementById("loginButton");
+
+const signupFunction = () => {
+  let signupSection = document.getElementById("signupSection");
+  signupSection.classList.remove("hidden");}
