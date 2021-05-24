29f604e18740868e7d449ad257084f5585df81dc
97hdz <70217263+97hdz@users.noreply.github.com>
2021-05-24 19:30:27 +0200

    Add files via upload

--- JS To-Do-List/main.js
+++ JS To-Do-List/main.js
@@ -1,7 +1,38 @@
-//Listeners
+//Variables
 let signupButton = document.getElementById("signupButton");
 let loginButton = document.getElementById("loginButton");
+let signupSection = document.getElementById("signupSection");
+let loginSection = document.getElementById("loginSection");
+const buttons = document.getElementById('index');
 
-const signupFunction = () => {
-  let signupSection = document.getElementById("signupSection");
-  signupSection.classList.remove("hidden");}
+//Functions to show the form
+let signupForm = (event) => {
+  signupSection.classList.remove("hidden")
+  buttons.classList.add("hidden");
+}
+
+let loginForm = (event) => {
+  loginSection.classList.remove("hidden")
+  buttons.classList.add("hidden");
+}
+
+
+//buttons to save all the data
+let saveInput = (event) => {
+  let name = document.getElementById('signupName').value;
+  let lastname = 'Lastname : ' + document.getElementById('signupLastname').value;
+  let email = 'Email : ' + document.getElementById('signupEmail').value;
+  let pass = 'password : ' + document.getElementById('signupPassword').value;
+  let old_nome = JSON.parse(localStorage.getItem(name));
+  localStorage.setItem(' Name : ', JSON.stringify(name));
+  console.log(name)
+}
+
+let goBack = (event) => {
+  window.location.reload();
+}
+
+
+//Listeners
+loginButton.addEventListener("click", loginForm);
+signupButton.addEventListener("click", signupForm);
