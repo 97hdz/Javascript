eb23e8af02e0f01fa90ae01b29da9da241b95e23
97hdz <70217263+97hdz@users.noreply.github.com>
2021-06-07 19:22:07 +0200

    sign up done

--- JS To-Do-List/main.js
+++ JS To-Do-List/main.js
@@ -5,8 +5,6 @@ let signupSection = document.getElementById("signupSection");
 let loginSection = document.getElementById("loginSection");
 const buttons = document.getElementById('index');
 
-//Values of
-
 //Functions to show the form depending on wich button you click
 let signupForm = (event) => {
   signupSection.classList.remove("hidden")
@@ -20,13 +18,30 @@ let loginForm = (event) => {
 
 //buttons to save all the data
 let saveInput = (event) => {
+  window.location.reload();
+
   let name = document.getElementById('signupName').value;
   let lastname = 'Lastname : ' + document.getElementById('signupLastname').value;
   let email = 'Email : ' + document.getElementById('signupEmail').value;
   let pass = 'password : ' + document.getElementById('signupPassword').value;
-  let old_nome = JSON.parse(localStorage.getItem(name));
-  localStorage.setItem(' Name : ', JSON.stringify(name));
-  alert("Registration complete");
+
+  //se non c'è niente da memorizzare all'inizio memorizza un array vuoto
+  if(localStorage.getItem('emailData')==null){
+    localStorage.setItem('emailData','[]');
+  }
+  if(localStorage.getItem('passData')==null){
+    localStorage.setItem('passData','[]');
+  }
+
+  //dati vecchi e aggiungi a quelli nuovi
+  let old_mail = JSON.parse(localStorage.getItem('emailData'));
+  old_mail.push(email);
+  let old_pass = JSON.parse(localStorage.getItem('passData'));
+  old_pass.push(pass);
+
+  //memorizza i dati vecchi + i nuovi dati
+  localStorage.setItem('emailData', JSON.stringify(old_mail));
+  localStorage.setItem('passData', JSON.stringify(old_pass));
 }
 
 
