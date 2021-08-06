473ec5696c5769be64f4a98985e776d87befc414
97hdz <70217263+97hdz@users.noreply.github.com>
2021-08-06 20:39:43 +0200

    v 0.0.4

--- Classes/project3.js
+++ Classes/project3.js
@@ -9,9 +9,8 @@
 // Functions ---> call(passengerFlor) = The passenger will be able to call the elevator to the floor they're on ex. call(7) if they're on the 7th floor
 //                                      the time will get the absolute value between the elevator's floor and the passenger's floor
 //                                      it will console Elevator floor, passengerFlor and time, and the boolean value isCall will be set to true
-//                up(goToUp) = This function will only work if the isCall value is set to true hence you have to first use the call() function, it will go
-//                             up by one, if you dont give any input to the function otherwise you can go to the floor you like, if use in default - just going
-//                             one floor up the time will increase just by one, if you choose the specific floor the absolute value of time will be calculated again
+//                up(goToUp) = This function will only work if the isCall value is set to true hence you have to first use the call() function, you can go to the floor you like,
+//                             when you choose the specific floor the absolute value of time will be calculated again
 //                down(goToDown) = Same functionalities of the up() function but in this case the elevator will be going down
 //
 //
@@ -30,6 +29,10 @@ class Elevator {
     this.time = 0;
     this.isCall;
     this.elevator ;
+    this.emergencyFloor ;
+  }
+  emergencyAt(floor){
+    this.emergencyFloor = floor;
   }
   call(passengerFloor){
     this.time = Math.abs(passengerFloor-this.currentFloor);
@@ -38,17 +41,30 @@ class Elevator {
     this.isCall = true;
   }
   up(goToUp){
-    for (var i = this.currentFloor; i <= goToUp; i++) {
-      console.log(`${this.elevator} is going up, currently at ${this.currentFloor}`);
-      this.currentFloor += 1;
-      this.time += 1;
+    if (goToUp > this.currentFloor ) {
+      for (var i = this.currentFloor; i <= goToUp; i++) {
+        if (this.emergencyFloor == i) { console.log(`Emergency at ${i} button pressed, open doors`); break; }
+        console.log(`${this.elevator} is going up, currently at ${this.currentFloor}`);
+        this.currentFloor += 1;
+        this.time += 1;
+      }
+      console.log(`total time : ${this.time} seconds to go arrived at ${this.currentFloor-1} `);
+    } else {
+      console.log(`You may want to use the DOWN button`);
     }
-    console.log(`total time : ${this.time}`);
   }
   down(goToDown){
-    console.log(`${this.elevator} is going up, currently at ${this.currentFloor-1}`);
-    this.currentFloor -= 1;
-    this.time += 1;
+    if (goToDown < this.currentFloor) {
+      for (var i = this.currentFloor; i >= goToDown; i--) {
+        if (this.emergencyFloor == i) { console.log(`Emergency at ${i} button pressed, open doors`); break; }
+        console.log(`${this.elevator} is going down, currently at ${this.currentFloor}`);
+        this.currentFloor -= 1;
+        this.time += 1;
+      }
+      console.log(`total time : ${this.time} seconds to go arrived at ${this.currentFloor+1} floor`);
+    } else {
+      console.log(`You may want to use the UP button`);
+    }
   }
 }
 
@@ -69,10 +85,10 @@ class ElevatorA extends Elevator {
   }
   up(goToUp){
     if (this.isCall) {
-      if (this.currentFloor < 9) {
+      if (this.currentFloor < 9 ) {
         super.up(goToUp)
       } else {
-        console.log(` ERROR : you're currently at '${this.currentFloor} : The Pentahouse' you can only go down`);
+        console.log(` ERROR : you're currently at '${this.currentFloor} : Floor ' you can't acces the Pentahouse`);
       }
     } else {
       console.log(`${this.elevator} has not been called`);
@@ -93,14 +109,51 @@ class ElevatorA extends Elevator {
 
 // Elevator B
 class ElevatorB extends Elevator {
-  constructor(floors,currentFloor) {
-    super(floors,currentFloor);
+  constructor(floors,currentFloor,elevator) {
+    super(floors,currentFloor,elevator);
     this.floors.unshift(-1);
+    this.elevator = 'Elevator B';
+    // use random to place the elevator in a random floor
+    const random = Math.floor(Math.random() * this.floors.length);
+    this.currentFloor = this.floors[random];
+  }
+  call(passengerFloor){
+    super.call(passengerFloor)
+    console.log(`the elevetor A is now at : ${passengerFloor}`);
+    this.currentFloor = passengerFloor;
+  }
+  up(goToUp){
+    if (this.isCall) {
+      if (this.currentFloor < 10) {
+        super.up(goToUp)
+      } else {
+        console.log(` ERROR : you're currently at '${this.currentFloor} : The Pentahouse' you can only go down`);
+      }
+    } else {
+      console.log(`${this.elevator} has not been called`);
+    }
+  }
+  down(goToDown){
+    if (this.isCall) {
+      if (this.currentFloor > -1 ) {
+        super.down(goToDown)
+      } else {
+        console.log(` ERROR : you're currently at '${this.currentFloor} : Floor ' you can't access the Basement`);
+      }
+    } else {
+      console.log(`${this.elevator} has not been called`);
+    }
   }
 }
 
 const person1 = new ElevatorA ();
 const person2 = new ElevatorB ();
 
-person1.call(1);
-person1.up(3);
+
+person1.call(9);
+person1.emergencyAt(2);
+person1.down(1);
+console.log('-----------------------------------------------');
+person2.call(2);
+person2.up(10);
+person2.down(3);
